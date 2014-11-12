db30fd2f172d38b3f0b651daf038d5add218051c
Fabricio Orrala P <fabricio.orrala@gmail.com>
2014-11-12 18:09:18 -0500

    layou base

---
+++ src/YoExisto/ContenidoBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YoExisto\ContenidoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('YoExistoContenidoBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function homeAction()
+    {
+        return $this->render('YoExistoContenidoBundle:Templates:home.html.twig');
+    }
+
+    public function registrarAction()
+    {
+        return $this->render('YoExistoContenidoBundle:Templates:registrar.html.twig');
+    }
+
+    public function dashboardAction()
+    {
+        return $this->render('YoExistoContenidoBundle:Templates:dashboard.html.twig');
+    }
+}
---
+++ src/YoExisto/ContenidoBundle/YoExistoContenidoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace YoExisto\ContenidoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YoExistoContenidoBundle extends Bundle
+{
+}
