4d94143f89400bfc7dca3364fa107220946639f9
Fabricio Orrala P <fabricio.orrala@gmail.com>
2014-11-29 17:05:46 -0500

    a la entidad usuario puse el id como protected

--- src/YoExisto/ContenidoBundle/Entity/Usuario.php
+++ src/YoExisto/ContenidoBundle/Entity/Usuario.php
@@ -50,4 +50,19 @@ class Usuario extends BaseUser
     {
         return $this->cedula;
     }
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
