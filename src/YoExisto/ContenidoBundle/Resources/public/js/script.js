36a92e1ab898ab094dd9aaf8421bb8c13ff83ee1
Fabricio Orrala P <fabricio.orrala@gmail.com>
2014-11-29 16:29:46 -0500

    otro

--- src/YoExisto/ContenidoBundle/Resources/public/js/script.js
+++ src/YoExisto/ContenidoBundle/Resources/public/js/script.js
@@ -1,1 +1,62 @@
- 
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see a blank space instead of the map, this
+// is probably because you have denied permission for location sharing.
+
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    zoomControl: true,
+    scaleControl: false,
+    scrollwheel: false,
+  };
+  map = new google.maps.Map(document.getElementById('geomapa'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Encontrado.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+
+$(document).ready(function(){
+	google.maps.event.addDomListener(window, 'load', initialize);
+	
+	$('.btn-geo').click(function() {
+	});
+});
