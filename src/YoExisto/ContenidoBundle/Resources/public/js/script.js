5c5bd80b1eafad9d3bc603dc0d553f6cb6677671
Fabricio Orrala P <fabricio.orrala@gmail.com>
2014-12-05 17:22:42 -0500

    geolocalizacion corregida

--- src/YoExisto/ContenidoBundle/Resources/public/js/script.js
+++ src/YoExisto/ContenidoBundle/Resources/public/js/script.js
@@ -42,7 +42,42 @@ function setMap(lat, lng) {
     map.setCenter(map_location);
 }
 
-function initializeZ(zoomRecibido) {
+
+function initializeDir(address) {
+  // I create a new google maps object to handle the request and we pass the address to it
+  var geoCoder = new google.maps.Geocoder(address)
+  // a new object for the request I called "request" , you can put there other parameters to specify a better search (check google api doc for details) , 
+  // on this example im going to add just the address  
+  var request = {address:address};
+       
+  // I make the request 
+  geoCoder.geocode(request, function(result, status){
+    // as a result i get two parameters , result and status.
+    // results is an  array tha contenis objects with the results founds for the search made it.
+    // to simplify the example i take only the first result "result[0]" but you can use more that one if you want
+
+    // So , using the first result I need to create a  latlng object to be pass later to the map
+    var latlng = new google.maps.LatLng(result[0].geometry.location.lat(), result[0].geometry.location.lng());  
+ 
+    // some initial values to the map   
+    var myOptions = {
+      zoom: 15,
+      center: latlng,
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+         // the map is created with all the information 
+           var map = new google.maps.Map(document.getElementById("geomapa"),myOptions);
+
+         // an extra step is need it to add the mark pointing to the place selected.
+        var marker = new google.maps.Marker({position:latlng,map:map,title:'title'});
+ 
+  })
+}
+
+
+function initializeZ(zoomRecibido, lugarBuscado) {
   var mapOptions = {
     zoom: zoomRecibido,
     zoomControl: true,
@@ -52,26 +87,41 @@ function initializeZ(zoomRecibido) {
   map = new google.maps.Map(document.getElementById('geomapa'),
       mapOptions);
 
-  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+  if(!lugarBuscado){
+    // Try HTML5 geolocation
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
 
-      var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Encontrado.'
+        var infowindow = new google.maps.InfoWindow({
+          map: map,
+          position: pos,
+          content: 'Encontrado.'
+        });
+
+        map.setCenter(pos);
+
+        $("#form_latitud").val(position.coords.latitude);
+        $("#form_logitud").val(position.coords.longitude);
+
+
+      }, function() {
+        handleNoGeolocation(true);
       });
+    } else {
+      // Browser doesn't support Geolocation
+      handleNoGeolocation(false);
+    }
+  }else{
+    //buscar
 
-      map.setCenter(pos);
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
+    //alert("buscando en el mundo");
+    initializeDir(lugarBuscado);
+
+
   }
+
 }
 
 
@@ -121,7 +171,7 @@ $(document).ready(function(){
 	google.maps.event.addDomListener(window, 'load', initialize);
 	
 	$('.btn-geo').click(function() {
-		initializeZ(15);
+		initializeZ(15,  $("#form_descripcion").val()   );
 	});
 
 	mostrarPanel();
